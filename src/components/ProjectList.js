1fd66acb8056880ad61d31daaaa5ada14742393e
fahmy-dev <faahmy@proton.me>
2025-01-16 00:46:24 +0300

    initial commit

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,17 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projectItems = projects.map((projObj) => {
+    return <ProjectItem key={projObj.id} name={projObj.name} about={projObj.about} technologies={projObj.technologies}/>
+  })
+
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectItems}
+      </div>
     </div>
   );
 }
